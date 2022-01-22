7e53cbaecbc3c8880bddf887d1b1dbc8de01d54c
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-01-21 23:31:49 -0300

    Parte 04 cadastrar

---
+++ formCadastrar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Cadastrar uma pessoa</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <a href="index.php">Cancelar cadastro e voltar</a><br>
+  <form action="cadastrar.php" method="POST">
+    <fieldset>
+      <label for="nome">Nome: </label><br>
+      <input type="text" id="nome" name="nome" placeholder="Nome"><br>
+      <label for="idade">Idade: </label><br>
+      <input type="number" id="idade" name="idade" placeholder="Idade"><br>
+      <input class="botaoSubmit" type="submit" value="Cadastrar pessoa">
+    </fieldset>
+  </form>
+</body>
+</html>
